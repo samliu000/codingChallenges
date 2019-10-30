d4f65552de3e263decab593cb7b4c2f27fe83e00
samliu000 <samuel.liu1004@gmail.com>
2019-10-29 18:20:06 -0700

    Quick Sort

---
+++ src/codingchallenges/QuickSort.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codingchallenges;
+
+/**
+ *This class will have a method that when used, uses quick sort on an array
+ * @author samuelliu
+ */
+public class QuickSort {
+    public static void quickSort(int[] array){
+        
+    }
+}

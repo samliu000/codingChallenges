b368c62bacbd33d2f5126b4ad294e545a725c006
samliu000 <44206874+samliu000@users.noreply.github.com>
2019-11-26 10:07:06 -0800

    Tested for generic <T extends GrandParent & Rodent>

--- src/genericsPractice/GenericsPractice.java
+++ src/genericsPractice/GenericsPractice.java
@@ -11,10 +11,10 @@ package genericsPractice;
  */
 public class GenericsPractice<T> {
     public GenericsPractice(T variable){}
-    public static <T extends Parent> void test1(){}
-    public static <T extends Primate> void test2(){}
-    public static <T extends GrandParent & Rodent> void test3(){}
-    public static <T extends Parent & Supraprimate> void test4(){}
+    public static <T extends Parent> void test1(T var){}
+    public static <T extends Primate> void test2(T var){}
+    public static <T extends GrandParent & Rodent> void test3(T var){}
+    public static <T extends Parent & Supraprimate> void test4(T var){}
      
     public static void main(String args[]){
         
@@ -32,6 +32,25 @@ public class GenericsPractice<T> {
         practice = new GenericsPractice(mouse);
         
         //See if all classes work for generic <E extends Parent>
+        test1(oldman);
+        test1(mom);
+        test1(kid);
+        test1(monkey);
+        test1(mouse);
+        
+        //See if all classes work for generic <E extends Primate>
+        test2(oldman);
+        test2(mom);
+        test2(kid);
+        test2(monkey);
+        test2(mouse);
+        
+        //See if all classes work for generic <E extends Primate>
+        test3(oldman);
+        test3(mom);
+        test3(kid);
+        test3(monkey);
+        test3(mouse);
         
     }
 }

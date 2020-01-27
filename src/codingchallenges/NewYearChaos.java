60c44537ba3e88da252493ca69ae715a1a3a5cfa
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-01-27 11:50:57 -0800

    Changes to New Year Chaos

--- src/codingchallenges/NewYearChaos.java
+++ src/codingchallenges/NewYearChaos.java
@@ -1,4 +1,4 @@
-/*
+/* FROM HACKERRANK!!!
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,5 +10,21 @@ package codingchallenges;
  * @author boo13
  */
 public class NewYearChaos {
-    
+     static void minBribeHelper(int[] q, int count) {
+        boolean sorted = true;
+        for(int i = 1; i <= q.length; i++){
+            if(q[i] != i) {
+                sorted = false;
+            }
+        }
+
+        if(sorted) {
+
+        }
+
+    }
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        
+    }
 }

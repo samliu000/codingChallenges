a0a53bf9e288408a7c431e993c4c5a0caf2bf12c
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-01-28 20:32:18 -0800

    New Year Chaos

--- src/codingchallenges/NewYearChaos.java
+++ src/codingchallenges/NewYearChaos.java
@@ -1,7 +1,15 @@
-/* FROM HACKERRANK!!!
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* It's New Year's Day and everyone's in line for the Wonderland rollercoaster 
+ * ride! There are a number of people queued up, and each person wears a sticker 
+ * indicating their initial position in the queue. Initial positions increment 
+ * by  from  at the front of the line to  at the back.
+
+ * Any person in the queue can bribe the person directly in front of them to 
+ * swap positions. If two people swap positions, they still wear the same sticker 
+ * denoting their original places in line. One person can bribe at most two others.
+ * For example, if  and  bribes , the queue will look like this: .
+
+ * Fascinated by this chaotic queue, you decide you must know the minimum number of
+ * bribes that took place to get the queue into its current state!
  */
 package codingchallenges;
 
